7581ca6a509cebb825d8f81fa2344d26ee418e71
CaioLucky <luckyechica@gmail.com>
2025-05-14 14:35:16 -0300

    Add files via upload

---
+++ Objetoss.js
@@ -0,0 +1,17 @@
+let celular = {
+    celular: "Sansung Galaxy S20 FE",
+    tela: "Super AMOLED",
+    memoria_ram: "8GB",
+    bateria:"4.500 mAh",
+    processador: "Exynos 990"
+}
+
+let celulares = [
+    {celular: "Sansung Galaxy S20 FE", tela: "Super AMOLED", memoria_ram: "8GB", bateria: "4.500 mAh", processador: "Exynos 990"},
+    {celular: "Iphone 16", tela: "Super Retina XDR Oled", memoria_ram: "8GB", bateria: "3561mAh", processador: "A18 Pro"},
+    {celular: "Sansung Galaxy S24", tela: "Dynamic AMOLED", memoria_ram: "8GB", bateria: "4.000 mAh", processador:"Exynos 2400"}
+
+]
+
+console.log(celulares[1].processador)
+console.log(celulares[0].tela)

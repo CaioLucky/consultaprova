81c3affd802773d40426e2b091ba83338c9684cf
CaioLucky <luckyechica@gmail.com>
2025-05-27 14:53:31 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,65 +1,47 @@
 const express = require('express')
-const mysql = require("mysql2")
-const cors = require('cors')
-
 
 const app = express()
 
-app.use(cors())
+app.get('/', (req, res) =>  {
+    res.send("Servidor Express Funcionando!!")
+})
+
+app.listen(3000,() =>{
+    console.log ("Servidor backend rodando em http://localhost:3000")
+})
 
 app.use(express.json())
 
-const conexao = mysql.createConnection({
-    host: "localhost",
-    user:"root",
-    password: "",
-    database:"loja",
-})
-
-
 const produtos = []
 
-app.post('/produtos', (req, res) => {
+app.post("/produtos", (req, res) => {
     const produto = {
         nome: req.body.nome,
-        preco: req.body.preco,
-        quantidade: req.body.quantidade,
+        preco:  req.body.preco,
+        quantidade:  req.body.quantidade,
     }
 
-    if (!produto.nome || typeof produto.nome != 'string' || produto.nome.trim() == '') {
-        return res.status(400).send('Nome do produto é obrigatório e deve ser uma string não vazia.');
-    }
+    produtos.push(produto)
 
-    if (produto.preco == undefined || typeof produto.preco != 'number' || produto.preco <= 0) {
-        return res.status(400).send('Preço deve ser um número positivo.');
-    }
-
-    if (produto.quantidade == undefined || !Number.isInteger(produto.quantidade) || produto.quantidade < 0) {
-        return res.status(400).send('Quantidade deve ser um número inteiro maior ou igual a 0.');
-    }
-
-    conexao.query(
-        "INSERT INTO produtos (nome,preco,quantidade) VALUES (?,?,?)",
-        [produto.nome, produto.preco, produto.quantidade],
-        ()=> {
-            res.status(201).send("Produtos cadastrados com sucesso!")
-        }
-    )
-
-   
-
-    res.status(201).send('Produtos cadastrado com sucesso!')
-
+    res.send("Produtos cadastrados com sucesso!")
 })
 
-app.get('/produtos', (req, res) => {
-    res.status(200).send(produtos)
+app.get("/produtos", (req, res) =>{
+    res.send(produtos)
 })
 
-app.listen(3000, () => {
-    console.log("Servidor backend rodando em http://localhost:3000")
-})
+if(!produto.nome || typeof produto.nome != "string"|| produto.nome.trim()=='') {
+    return res.status(400).send("Nome do produto é obrigatório e deve ser uma string não vazia.")
+}
 
+if (produto.preco== undefined || typeof produto.preco != "number" || produto.preco <=0){
+return res.status(400).send("Preço deve ser um número positivo.")
+}
 
+if (produto.quantidade == undefindes || !Number.isInteger(produto.quantidade)|| produto.quantidade<0){
+    return res.status(400).send("Quantidade deve ser um númeroninteiro maior ou igual a 0.")
+}
 
-app.use(cors())
+produtos.push(produto)
+
+res.status(201).send("Produtos cadastrados com sucesso!")
